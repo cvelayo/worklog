76de590050accd087ad91c7257cc5d751180291f
Christopher Velayo <cvelayo@gmail.com>
2016-01-08 08:59:34 -0700

    Last MySQL version
    last mysql version before shifting to mssql

--- report.php
+++ report.php
@@ -1484,7 +1484,11 @@ if ($phase == 2)
 
         }
         var_dump($hours);
-        echo array_sum(array_column($hours['f2f']['SW'], 'staff'));
+        echo "Social Work face to face hours: " . array_sum($hours['f2f']['SW'])/3600 . " hours.";
+        $team = array('SW', 'Psychology', 'C4C');
+        foreach ($team as $key)
+        {
+          echo ($hours['f2f'][$key]['intern'])/3600 . " hours";}
         do_summary($count, $hours, $room_hash, $name_hash);
       }
       else
